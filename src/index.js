266d6c20d1c28a4e2d4050daf46fc442cbfe6e93
RGBKnights <master@rgbknights.com>
2018-10-11 16:04:03 -0400

    removed the server worker cause its getting in the way...

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './client/app';
-import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './client/app.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
