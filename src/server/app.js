06bd3b92a1b0aed4473024ab0d2cf72a4d0c8956
RGBKnights <master@rgbknights.com>
2018-10-16 14:39:15 -0400

    Reset for testing

--- src/server/app.js
+++
@@ -1,9 +0,0 @@
-import { Server } from 'boardgame.io/server';
-import GameCore from '../game/core';
-
-const server = Server({ games: [GameCore] })
-
-const port = process.env.PORT || 8000;
-console.log(`App running on http://localhost:${port}/`)
-console.log(`Api running on http://localhost:${port+1}/`)
-server.run(port);
